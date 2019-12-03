171d2c72ea2eb350652a3d4826874339d081e433
Nicolas Jourdain <nicolas.jourdain@randstad.ca>
2019-12-02 23:52:54 -0500

    Day 2

---
+++ day2/part1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var fileContent = await File.ReadAllLinesAsync("input.csv");
+            var program = fileContent[0].Split(",").Select(Int32.Parse).ToArray();
+
+            var result = Run(program, 12, 2);
+
+            Console.WriteLine($"Position 0 has value {program[0]}");
+        }
+
+        public static int Run(int[] program, int noun, int verb)
+        {
+            program[1] = noun;
+            program[2] = verb;
+
+            for (var i = 0; i < program.Length; i += 4)
+            {
+                if (program[i] == 1)
+                {
+                    program[program[i + 3]] = program[program[i + 1]] + program[program[i + 2]];
+                }
+                else if (program[i] == 2)
+                {
+                    program[program[i + 3]] = program[program[i + 1]] * program[program[i + 2]];
+                }
+                else if (program[i] == 99)
+                {
+                    break;
+                }
+            }
+
+            return program[0];
+        }
+    }
+}
---
+++ day2/part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var fileContent = await File.ReadAllLinesAsync("input.csv");
+            var program = fileContent[0].Split(",").Select(Int32.Parse).ToArray();
+
+            for (var noun = 0; noun < 100; noun++)
+            {
+                for (var verb = 0; verb < 100; verb++)
+                {
+                    if (part1.Program.Run(program.ToArray(), noun, verb) == 19690720)
+                    {
+                        Console.WriteLine($"Result is {100 * noun + verb}");
+                    }
+                }
+            }
+
+        }
+    }
+}
