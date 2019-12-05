2ec6b082cc78c763f6c7c5e280fdd08fcbeb8192
Nicolas Jourdain <nicolas.jourdain@randstad.ca>
2019-12-04 23:44:40 -0500

    Day 4

---
+++ day4/part1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            for (var i = 402328; i <= 864247; i++)
+            {
+                var number = i.ToString();
+                var hasDoubleDigit = false;
+                var hasDecreasing = false;
+
+                for (var j = 0; j < 5; j++)
+                {
+                    hasDoubleDigit |= number[j] == number[j + 1];
+
+                    if (number[j] > number[j + 1])
+                    {
+                        hasDecreasing = true;
+                        break;
+                    }
+                }
+
+                if (hasDoubleDigit && !hasDecreasing)
+                    count++;
+            }
+
+            Console.WriteLine($"{count} possible passwords");
+        }
+    }
+}
---
+++ day4/part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            for (var i = 402328; i <= 864247; i++)
+            {
+                var number = i.ToString();
+                var hasDecreasing = false;
+
+                for (var j = 0; j < 5; j++)
+                {
+                    if (number[j] > number[j + 1])
+                    {
+                        hasDecreasing = true;
+                        break;
+                    }
+                }
+
+                if (hasDecreasing)
+                    continue;
+
+                if (number.GroupBy(x => x).Any(x => x.Count() == 2))
+                    count++;
+            }
+
+            Console.WriteLine($"{count} possible passwords");
+        }
+    }
+}
